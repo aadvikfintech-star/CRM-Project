a09277e2b1e5e69189b533e11276e5f264a589ad
Aadvik Techie Views <aadvikfintech@gmail.com>
2026-02-22 18:41:56 +0530

    Add additional CORS origin for production

--- crm-project-main/crm_backend/crm_service/src/main/java/com/org/crm_service/config/CorsConfig.java
+++ crm-project-main/crm_backend/crm_service/src/main/java/com/org/crm_service/config/CorsConfig.java
@@ -1,24 +1,31 @@
 package com.org.crm_service.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
+
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://crm-project-2.onrender.com"
+        ));
+
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
-
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
+
         source.registerCorsConfiguration("/**", config);
 
         return source;
